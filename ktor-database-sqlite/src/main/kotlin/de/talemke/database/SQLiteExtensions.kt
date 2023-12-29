9e76e9386832ff8ebdc4cddce694ec729259c227
Tassilo Lemke <tassilo.lemke@gmail.com>
2023-12-29 22:32:34 +0100

    docs: Improve documentation for ktor-database-sqlite

--- ktor-database-sqlite/src/main/kotlin/de/talemke/database/SQLiteExtensions.kt
+++ ktor-database-sqlite/src/main/kotlin/de/talemke/database/SQLiteExtensions.kt
@@ -3,18 +3,38 @@ package de.talemke.database
 import org.jetbrains.exposed.sql.DatabaseConfig
 import java.io.File
 
+/**
+ * Adds an SQLite database.
+ *
+ * @param file the database file
+ * @param configurator optional configuration
+ */
 fun DatabasePluginConfigurator.addSQLite(file: File, configurator: DatabaseConfig.Builder.() -> Unit) {
 	addDatabase {
 		createSQLite(file, configurator)
 	}
 }
 
+/**
+ * Adds an SQLite database with the specified key.
+ *
+ * @param key the database key
+ * @param file the database file
+ * @param configurator optional configuration
+ */
 fun DatabasePluginConfigurator.addSQLite(key: String, file: File, configurator: DatabaseConfig.Builder.() -> Unit) {
 	addDatabase(key) {
 		createSQLite(file, configurator)
 	}
 }
 
+/**
+ * Adds an SQLite database with the specified key.
+ *
+ * @param key the database key
+ * @param file the database file
+ * @param configurator optional configuration
+ */
 inline fun <reified E : Enum<E>> DatabasePluginConfigurator.addSQLite(key: E, file: File, noinline configurator: DatabaseConfig.Builder.() -> Unit) {
 	addDatabase(key) {
 		createSQLite(file, configurator)
