04597ed6007d9468e3ccb2883e34dca70c2141c5
Tassilo Lemke <tassilo.lemke@gmail.com>
2023-12-29 22:36:09 +0100

    docs: Improve documentation for ktor-database-mysql

--- ktor-database-mysql/src/main/kotlin/de/talemke/database/MySQLExtensions.kt
+++ ktor-database-mysql/src/main/kotlin/de/talemke/database/MySQLExtensions.kt
@@ -2,18 +2,41 @@ package de.talemke.database
 
 import com.zaxxer.hikari.HikariConfig
 
+/**
+ * Adds a remote MySQL database.
+ *
+ * @param hostname the hostname of the server
+ * @param database the database name
+ * @param configurator further configuration
+ */
 fun DatabasePluginConfigurator.addMySQL(hostname: String, database: String, configurator: HikariConfig.() -> Unit) {
 	addDatabase {
 		createMySQL(hostname, database, configurator)
 	}
 }
 
+/**
+ * Adds a remote MySQL database with the given key.
+ *
+ * @param key the database key
+ * @param hostname the hostname of the server
+ * @param database the database name
+ * @param configurator further configuration
+ */
 fun DatabasePluginConfigurator.addMySQL(key: String, hostname: String, database: String, configurator: HikariConfig.() -> Unit) {
 	addDatabase(key) {
 		createMySQL(hostname, database, configurator)
 	}
 }
 
+/**
+ * Adds a remote MySQL database with the given key.
+ *
+ * @param key the database key
+ * @param hostname the hostname of the server
+ * @param database the database name
+ * @param configurator further configuration
+ */
 inline fun <reified E : Enum<E>> DatabasePluginConfigurator.addMySQL(key: E, hostname: String, database: String, configurator: HikariConfig.() -> Unit) {
 	addDatabase(key) {
 		createMySQL(hostname, database, configurator)
